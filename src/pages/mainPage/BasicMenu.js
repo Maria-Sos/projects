d8ce588a3a1f0d02e863ea13851eb64e37f76fb5
mariaSOS <ms2145051478@gmail.com>
2023-05-07 12:43:53 -0500

    added new js vanilla project

--- src/pages/mainPage/BasicMenu.js
+++ src/pages/mainPage/BasicMenu.js
@@ -25,6 +25,7 @@ export default function BasicMenu() {
 		  <li><a className="dropdown-item"  href="https://advocates.glitch.me" style={linkStyle} target="_blank">Advocate web page</a></li>
 		  <li><h5 className="dropdown-header">JS Vanilla</h5></li>
 		  <li><a className="dropdown-item"  href="https://to-do-vanilla.glitch.me" style={linkStyle} target="_blank">To Do List</a></li>
+		  <li><a className="dropdown-item"  href="https://create-your-menu.glitch.me/" style={linkStyle} target="_blank">Order your menu</a></li>
 		  <li><hr className="dropdown-divider"/></li>
 		  <li><h5 className="dropdown-header">React projects</h5></li>
 		  <li><a className="dropdown-item" href="https://g-memory.netlify.app/" style={linkStyle}>Memory Game</a></li>
